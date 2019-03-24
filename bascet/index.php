052084557223f166fb04cf56d3b9b7dc116ac624
dmtrmkhlv <app@dmtrmkhlv.ru>
2019-03-24 20:58:08 +0300

    Bascet

--- bascet/index.php
+++ bascet/index.php
@@ -13,7 +13,7 @@
 
     session_start();
     $template = [
-      'products' =>[]
+      'product' =>[]
     ];
 
     foreach($_SESSION['bascet'] as $bascetItem){
@@ -21,14 +21,21 @@
     $result = mysqli_query($db, $sql);
     $product = mysqli_fetch_assoc($result);
     //comments
-    $template['products'][] = $products;
+    $template['product'][] = $product;
     }
+    // print_r($_SESSION);
+
 ?>
-    <main>
-        <h1 class="main-header">КОРЗИНА</h1>
-        
-    
-    </main>
+     <main class="main">
+            <h1 class="main-header">ВАША КОРЗИНА</h1>
+            <h3>Товары резервируются на ограниченное время</h3>
+                <?php foreach($template['product'] as $productItem):?>
+                <div style='background-image:url(<?=$productItem['photo']?>)'></div>
+                <div><?=$productItem['name']?></div>
+                <div><?=$productItem['price']?></div>
+                <div><?=$productItem['description']?></div>
+                <?php endforeach; ?>
+        </main>
 <?php
     include($_SERVER['DOCUMENT_ROOT'].'/parts/footer.php');
 ?>
